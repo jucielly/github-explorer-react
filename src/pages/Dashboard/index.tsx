cc89574ba7a5090864aa8bba3f46b08fcee8cfff
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-07-21 18:15:31 -0300

    pages and routes config

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { exception } from 'console';
+
+const Dashboard: React.FC = () => {
+    return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { exception } from 'console';
+
+const Repository: React.FC = () => {
+    return <h1>Repository</h1>;
+};
+
+export default Repository;

62fde83e2b7cb838738c3ea9af349c68ab6d8bdb
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-09-16 16:40:46 -0300

    added style to the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState } from 'react';
+import LogoImg from '../../assets/Logo.svg';
+import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from "react-icons/fi";
+
 
 const Dashboard: React.FC = () => {
-    return <Title>DAHSBOARD</Title>;
+
+
+    return (
+        <>
+            <img src={LogoImg} alt="logo" />
+            <Title>Explore Repositorios no Github</Title>
+
+            <Form>
+                <input placeholder="pesquisar repositorio" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+            <Repositories>
+                <a href="#">
+                    <div className="container">
+                        <img src="https://avatars1.githubusercontent.com/u/36751273?s=460&u=9c60260daea5ae5cc3880f0d8632c0065971827d&v=4" />
+                        <div className="info">
+                            <strong>Repository</strong>
+                            <p>description description</p>
+                        </div>
+                    </div>
+                    <FiChevronRight />
+                </a>
+            </Repositories>
+        </>
+    )
 };
 
 export default Dashboard;
